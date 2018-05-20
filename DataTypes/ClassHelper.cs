e585cf60a5413fe33d664ac9e0a5e4af26f84b8d
aaronamm <theeranitp@gmail.com>
2018-05-20 23:07:45 +0700

    help class to get property name by expression

---
+++ DataTypes/ClassHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace CodeSanook.Common.DataTypes
+{
+    public static class ClassHelper
+    {
+
+        public static string PropertyAsString<TObject, TProperty>(this TObject obj, Expression<Func<TObject, TProperty>> propertySelector)
+            where TObject : class
+        {
+            var memberExpression = propertySelector.Body as MemberExpression;
+            return memberExpression.Member.Name;
+        }
+    }
+}
