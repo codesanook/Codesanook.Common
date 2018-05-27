426c0846f667270749f63452a7b0efb3e3f86fe7
aaron <theeranitp@gmail.com>
2018-05-27 14:09:26 +0700

    report API setup SMTP for forgot password

---
+++ Web/JsonActionResult.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeSanook.Common.Web
+{
+    public class JsonActionResult : ActionResult
+    {
+        object model;
+        public JsonActionResult(object model)
+        {
+            this.model = model;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.HttpContext.Response;
+            response.ContentType = "application/json";
+            response.ContentEncoding = Encoding.UTF8;
+
+            var setting = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+            response.Write(JsonConvert.SerializeObject(model, setting));
+
+        }
+    }
+}
