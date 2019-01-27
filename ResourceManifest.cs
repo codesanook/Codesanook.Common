d3d0bbb8b325e56f043033add161f0e33bf16e01
aaronamm <theeranitp@gmail.com>
2019-01-27 21:13:12 +0700

    Define SystemJS resource in CodeSanook.Common module

---
+++ ResourceManifest.cs
@@ -0,0 +1,15 @@
+using Orchard.UI.Resources;
+
+namespace Orchard.Core.Common
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+            manifest
+                .DefineScript("SystemJS")
+                .SetUrl("system.js");
+        }
+    }
+}
