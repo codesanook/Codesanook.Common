ccddc9a31f668a0ca4efb0a221053372e1ac14c0
aaronamm <theeranitp@gmail.com>
2019-06-08 12:04:41 +0700

    Register ReactEnvironment with Autofac

---
+++ ShellEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Environment;
+using React;
+
+namespace Codesanook.Common {
+    public class ShellEvent : IOrchardShellEvents {
+        public void Activated() {
+            
+            ReactSiteConfiguration.Configuration
+                .SetLoadBabel(false)
+                .AddScriptWithoutTransform("~/modules/codesanook.common/scripts/server-bundle.js");
+        }
+
+        public void Terminating() {
+        }
+    }
+}
