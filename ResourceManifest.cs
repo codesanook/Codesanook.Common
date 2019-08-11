3b80bf1d7ed5d33add57b7244a7f86fc698e16cd
aaronamm <theeranitp@gmail.com>
2019-08-11 10:36:55 +0700

    Remove ReactJS.NET

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -9,8 +9,6 @@ namespace Orchard.Core.Common
             var manifest = builder.Add();
             manifest.DefineScript("SystemJS").SetUrl("system.js");
             manifest.DefineScript("Lodash").SetUrl("lodash.js");
-            manifest.DefineScript("React").SetUrl("react.development.js");
-            manifest.DefineScript("ReactDOM").SetUrl("react-dom.development.js");
             manifest.DefineScript("AngularMessages").SetUrl("angular-messages.js");
         }
     }
